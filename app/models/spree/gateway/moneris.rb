347e0f01e389373ce809ee0904afcddea22a293c
Frédéric boivin <fboivin63@gmail.com>
2015-09-23 17:52:29 -0400

    cancel

--- app/models/spree/gateway/moneris.rb
+++ app/models/spree/gateway/moneris.rb
@@ -6,5 +6,22 @@ module Spree
     def provider_class
       ActiveMerchant::Billing::MonerisGateway
     end
+
+    def cancel(response_code)
+      p = Spree::Payment.find_by(response_code: response_code)
+
+      puts "###############################################
+
+
+
+
+
+
+
+
+      p.inspect"
+
+
+    end
   end
 end
