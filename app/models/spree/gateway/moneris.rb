108f7581de11e00937c6d7d288f117b78071d6a7
dangerdogz <fboivin63@gmail.com>
2015-10-21 16:45:46 -0400

    add basic payment profile features to moneris

--- app/models/spree/gateway/moneris.rb
+++ app/models/spree/gateway/moneris.rb
@@ -7,9 +7,43 @@ module Spree
       ActiveMerchant::Billing::MonerisGateway
     end
 
-    def cancel(response_code)
-      provider.void(response_code, {})
+    def cancel(response_code, source, options = {})
+      response = provider.void(response_code, options)
+
+      unless response.success?
+        payment.send(:gateway_error, response)
+      end
     end
-    
+
+    def void(response_code, source, options = {})
+      response = provider.void(response_code, options)
+    end
+
+    def authorize(money, source, options = {})
+      if source.gateway_customer_profile_id.nil?
+        provider.authorize(money, source, options )
+      else
+        provider.authorize(money, source.gateway_customer_profile_id, options )
+      end
+    end
+
+    def credit(credit_cents, source, transaction_id, options = {})
+      provider.credit(credit_cents, transaction_id, options )
+
+    end
+
+    def payment_profiles_supported?
+      true
+    end
+
+    #Customer profile
+
+    def create_profile(payment)
+      if payment.source.gateway_customer_profile_id.nil?
+        response = provider.store(payment.source)
+        payment.source.update_attributes(gateway_customer_profile_id: response.params['data_key'], gateway_payment_profile_id: response.params['data_key'])
+      end
+    end
+
   end
 end
