7acb8c9609519dcd584df65ac6194b9172eb3555
dangerdogz <fboivin63@gmail.com>
2015-10-22 15:26:18 -0400

    more robust support for profile

--- app/models/spree/gateway/moneris.rb
+++ app/models/spree/gateway/moneris.rb
@@ -7,16 +7,12 @@ module Spree
       ActiveMerchant::Billing::MonerisGateway
     end
 
-    def cancel(response_code, source, options = {})
-      response = provider.void(response_code, options)
-
-      unless response.success?
-        payment.send(:gateway_error, response)
-      end
+    def cancel(response_code)
+      provider.void(response_code, {})
     end
 
     def void(response_code, source, options = {})
-      response = provider.void(response_code, options)
+      provider.void(response_code, options)
     end
 
     def authorize(money, source, options = {})
@@ -28,8 +24,7 @@ module Spree
     end
 
     def credit(credit_cents, source, transaction_id, options = {})
-      provider.credit(credit_cents, transaction_id, options )
-
+      provider.refund(credit_cents, transaction_id, options )
     end
 
     def payment_profiles_supported?
@@ -41,7 +36,11 @@ module Spree
     def create_profile(payment)
       if payment.source.gateway_customer_profile_id.nil?
         response = provider.store(payment.source)
-        payment.source.update_attributes(gateway_customer_profile_id: response.params['data_key'], gateway_payment_profile_id: response.params['data_key'])
+        if response.success?
+          payment.source.update_attributes(gateway_customer_profile_id: response.params['data_key'], gateway_payment_profile_id: response.params['data_key'])
+        else
+          payment.send(:gateway_error, response)
+        end
       end
     end
 
